dd0f53a7275951300e82731a9c3212a6f684bb3e
JosekCode <josepvillak@gmail.com>
2025-03-18 17:46:15 -0500

    Implementación de la función para el sortear
    Validar que haya amigos disponibles.
    Generar un índice aleatorio; Usando "Math.random() y Math.floor()"
    Obtener el nombre sorteado; Utilizando el índice aleatorio
    Mostrar el resultado; utilizando "document.getElementById()"  e "innerHTML"

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function agregarAmigo() {
     const nombre = document.getElementById('amigo').value.trim();
     const regex = /^[A-Za-z]+(\s[A-Za-z]+)*$/;
     if (nombre === '' || !regex.test(nombre)) {
-        alert('Por favor, ingrese un nombre válido (solo letras y espacios entre palabras)');
+        alert('Por favor, ingrese un nombre válido');
         return;
     }
     amigos.push(nombre);
@@ -23,4 +23,22 @@ function mostrarListaAmigos() {
         li.textContent = amigo;
         listaAmigos.appendChild(li);
     });
+}
+
+// 4. Implementa una función para sortear los amigos
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Por favor, agregue al menos un amigo antes de sortear.');
+        return;
+    }
+    const amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    mostrarResultado(amigoSorteado);
+}
+
+function mostrarResultado(amigo) {
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+    const li = document.createElement('li');
+    li.textContent = `El amigo secreto es: ${amigo}`;
+    resultado.appendChild(li);
 }
